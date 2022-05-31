b4269ecf16c0f0a28e1c2f1676aa618d213560e9
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-05-31 17:37:09 +0100

    g

--- notesview.js
+++ notesview.js
@@ -1,14 +1,15 @@
 class NotesView {
   constructor (model) {
-    this.model = model.getNotes;
+    this.model = model;
   }
   displayNotes = () => {
+    this.model.getNotes().forEach(note => {
       let el = document.createElement("div");
       el.className = 'note';
-      el.innerText = "test note";
+      el.innerText = note;
       document.getElementById("main-container").append(el);
+    });
   };
-
 }
 
 module.exports = NotesView;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 console.log("the notes app is running");
+const NotesView = require('../notesview');
 const NotesModel = require('./notesModel');
 
 const model = new NotesModel();
 
 console.log(model.getNotes());
 
+const notesView = new NotesView(model);
+
+model.addNote("this is so exciting I can barely cope");
+
+notesView.displayNotes();
